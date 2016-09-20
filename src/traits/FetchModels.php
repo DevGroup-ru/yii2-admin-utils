c68d53e9b247724a5d2b689ada2e1fb3467b34dc
Dmitiry Ronzhin <hector.tmb@gmail.com>
2016-09-20 13:19:38 +0300

    Update FetchModels.php
    fix on php7

--- src/traits/FetchModels.php
+++ src/traits/FetchModels.php
@@ -25,8 +25,10 @@ trait FetchModels
         }
         $outParams = [];
         $toClass = get_class($toModel);
-        $fromName = array_pop(explode('\\', $fromClass));
-        $toName = array_pop(explode('\\', $toClass));
+        $tmpFormNameArray = explode('\\', $fromClass);
+        $fromName = array_pop($tmpFormNameArray);
+        $tmpToNameArray = explode('\\', $toClass);
+        $toName = array_pop($tmpToNameArray);
         if (true === isset($params[$fromName])) {
             foreach ($params[$fromName] as $key => $value) {
                 if (true === in_array($key, $toModel->attributes(), true)) {
